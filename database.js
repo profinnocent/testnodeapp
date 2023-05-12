1e73043da2ad9229cb44abd658188df902f84592
profinnocent <innocent.unachukwu@gmail.com>
2023-05-12 08:13:22 -0700

    updated server file

--- database.js
+++ database.js
@@ -1,23 +1,21 @@
-import mysql from 'mysql2';
-import dotenv from 'dotenv';
+import mysql from "mysql2";
+import dotenv from "dotenv";
 dotenv.config();
 
 const connection = mysql.createConnection({
-    host: process.env.DBHOST,
-    user: process.env.DBUSER,
-    password: process.env.DBPASS,
-    database: process.env.DBNAME
-})
+  host: process.env.DBHOST,
+  user: process.env.DBUSER,
+  password: process.env.DBPASS,
+  database: process.env.DBNAME,
+});
 
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-  });
-
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 
 // Get all records from notes table
 function getNotes() {
-  let row = [];
   connection.query("SELECT * FROM notes", function (err, result) {
     if (err) throw err;
     console.log(result);
@@ -31,67 +29,77 @@ function getNotes() {
 // resArr['result'] = getNotes();
 // console.log(resArr);
 
-
 // Get a single record from notes table
 function getNote(id) {
-  connection.query("SELECT * FROM notes WHERE note_id=?",[id], function(err, result) {
-    if (err) throw err;
-    console.log(result);
-  });
+  connection.query(
+    "SELECT * FROM notes WHERE note_id=?",
+    [id],
+    function (err, result) {
+      if (err) throw err;
+      console.log(result);
+    }
+  );
 }
-  
-  // console.log(getNote(1));
-  // getNote(1);
+
+// console.log(getNote(1));
+// getNote(1);
 
 // Insert or Create a new record in notes table
 function createNotes(mytitle, mycontent) {
-      connection.query("INSERT INTO notes (title, contents) VALUES(?, ?)", [mytitle, mycontent], function (err, result) {
-        if (err) throw err;
-        console.log(result.insertId);
-      });
-  }
-  
-  // createNotes("Note 5", "This is note 5 content");
-
-
-  // Edit or Update a record in notes table
-function updateNote(mytitle, mycontent, myid) {
-
-    // Check if record exist
-    // const note = getNote(myid);
-    // console.log(note);
-    // if (note){
-      connection.query("UPDATE notes SET title=?, contents=? WHERE note_id=?",[mytitle, mycontent, myid], function (err, result) {
+  connection.query(
+    "INSERT INTO notes (title, contents) VALUES(?, ?)",
+    [mytitle, mycontent],
+    function (err, result) {
       if (err) throw err;
-      console.log(result.affectedRows + " record(s) updated");
-      });
-    // }
-    // else{
-    //   console.log("Note does not exist");
-    // }
-
+      console.log(result.insertId);
+    }
+  );
 }
-  
-  // updateNote("Note 22", "This is note 22 content", 2);
-  // getNote(2);
 
-// Delete a record in notes table
-function deleteNote(myid) {
+createNotes("Note 12", "This is note 12 content");
 
+// Edit or Update a record in notes table
+function updateNote(mytitle, mycontent, myid) {
   // Check if record exist
   // const note = getNote(myid);
   // console.log(note);
   // if (note){
-    connection.query("DELETE FROM notes WHERE note_id=?",[myid], function (err, result) {
-    if (err) throw err;
-    console.log(result.affectedRows + " record deleted");
-    });
+  connection.query(
+    "UPDATE notes SET title=?, contents=? WHERE note_id=?",
+    [mytitle, mycontent, myid],
+    function (err, result) {
+      if (err) throw err;
+      console.log(result.affectedRows + " record(s) updated");
+    }
+  );
   // }
   // else{
   //   console.log("Note does not exist");
   // }
-  
+}
+
+// updateNote("Note 22", "This is note 22 content", 2);
+// getNote(2);
+
+// Delete a record in notes table
+function deleteNote(myid) {
+  // Check if record exist
+  // const note = getNote(myid);
+  // console.log(note);
+  // if (note){
+  connection.query(
+    "DELETE FROM notes WHERE note_id=?",
+    [myid],
+    function (err, result) {
+      if (err) throw err;
+      console.log(result.affectedRows + " record deleted");
+    }
+  );
+  // }
+  // else{
+  //   console.log("Note does not exist");
+  // }
 }
 
 // deleteNote(3);
-getNotes();
+// getNotes();
--- test.js
+++ test.js
@@ -1,21 +1,144 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
 dotenv.config();
+
+// Import mysql crud functions
+import {
+  getNotes,
+  getNote,
+  createNote,
+  updateNote,
+  deleteNote,
+} from "./dbpool.js";
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!!!');
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World!!!");
 });
 
 // Get all notes
-app.get('/notes', (req, res) => {
-  
-  // res.send('Hello World!!!');
+app.get("/notes", async (req, res) => {
+  const notes = await getNotes();
+  if (notes.length > 0) {
+    res.status(200).send({ result: notes, resultCode: 1 });
+  } else {
+    res.status(400).send({ result: "No notes found", resultCode: 0 });
+  }
+});
+
+// Get a single note
+app.get("/notes/:id", async (req, res) => {
+  const note = await getNote(req.params.id);
+  if (note.length == 1) {
+    res.status(200).send({ result: note[0], resultCode: 1 });
+  } else if (note.length > 1) {
+    res
+      .status(400)
+      .send({ result: "Identical notes, pls check", resultCode: 2 });
+  } else {
+    res.status(400).send({ result: "No notes found", resultCode: 0 });
+  }
+});
+
+// Create a new note
+app.post("/notes", async (req, res) => {
+  const title = req.body.title;
+  const contents = req.body.contents;
+  const noteInsertId = await createNote(title, contents);
+  console.log(noteInsertId);
+  if (typeof noteInsertId === "number") {
+    res.status(200).send({
+      result: "Note inserted successfully",
+      noteInsertId: noteInsertId,
+      resultCode: 1,
+    });
+  } else {
+    res
+      .status(400)
+      .send({ result: "Note not created. Try again", resultCode: 0 });
+  }
+});
+
+// Update a note
+app.put("/notes/:id", async (req, res) => {
+  const id = req.params.id;
+
+  if (id === undefined || id === null || isNaN(id)) {
+    res
+      .status(400)
+      .send({ result: "Please provide a valid Note ID", resultCode: 0 });
+  } else {
+    const note = await getNote(id);
+    if (note.length === 1) {
+      // console.log(note);
+      const title =
+        req.body.title === undefined || req.body.title === null
+          ? note[0].title
+          : req.body.title;
+      const contents =
+        req.body.contents === undefined || req.body.contents === null
+          ? note[0].contents
+          : req.body.contents;
+
+      const result = await updateNote(title, contents, id);
+      // console.log(result);
+
+      if (result > 0) {
+        res.status(200).send({
+          result: "Note updated successfully",
+          resultCode: 1,
+        });
+      } else {
+        res.status(400).send({
+          result: "Note NOT updated. Try again",
+          resultCode: 0,
+        });
+      }
+    } else {
+      res.status(400).send({
+        result: "Note with this ID does not exist",
+        resultCode: 0,
+      });
+    }
+  }
+});
+
+// Delete a single Note (DELETE)
+app.delete("/notes/:id", async (req, res) => {
+  const id = req.params.id;
+
+  if (id === undefined || id === null || isNaN(id)) {
+    res
+      .status(400)
+      .send({ result: "Please provide a valid Note ID", resultCode: 0 });
+  } else {
+    const note = await getNote(id);
+    if (note.length === 1) {
+      const result = await deleteNote(id);
+      if (result) {
+        res.status(200).send({
+          result: "Note deleted successfully",
+          resultCode: 1,
+        });
+      } else {
+        res.status(400).send({
+          result: "Note NOT deleted. Try again",
+          resultCode: 0,
+        });
+      }
+    } else {
+      res.status(400).send({
+        result: "Note with this ID does not exist",
+        resultCode: 0,
+      });
+    }
+  }
 });
 
 app.listen(PORT, () => {
-  console.log('Example app listening on port:'+PORT);
+  console.log("Example app listening on port:" + PORT);
 });
